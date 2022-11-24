20c4934e8ac88df21cca127c91d94669fe67e8d4
in-drz <90423194+in-drz@users.noreply.github.com>
2022-11-23 17:16:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const config = {
   title: 'Mutual Aid Resources',
   description:
     'This is a tool to find resources in Los Angeles and the surrounding areas. You can find the nearest food pantries, fridges, or shelters with just a few clicks',
-  sideBarInfo: ['Location_Name', 'Address', 'Directions'],
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
   popupInfo: ['Location_Name'],
   filters: [
     {
